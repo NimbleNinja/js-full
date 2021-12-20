c1f2af0ffb2ad1f21943d3d98573f64990d61d9a
Oleg Zaporozhets <zaporozhets93@gmail.com>
2021-12-20 17:50:24 +0200

    lesson_4 task_4 v1.0

---
+++ lesson_4/task_4/script.js
@@ -0,0 +1,33 @@
+// task 1 =====================================
+
+// const n = 150;
+// let sum = 0;
+
+// for (let i = 1; i <= 150; i += 1) {
+//  sum += i;
+// }
+
+// console.log('Result: ' + sum);
+
+// task 2 ======================================
+
+// const m = 2;
+// const n = 22;
+// let result = 1;
+
+// for (let i = m; i <= n; i += 1) {
+//  if (i % 2 !== 0) {
+//    result *= i;
+//  }
+// }
+
+// console.log(result);
+
+// task 3 =======================================
+
+// for (let i = 1; i <= 5; i += 1) {
+//  console.log(`=> ${i} <=`);
+//  for (let j = 1; j <= 10; j += 1) {
+//    console.log(`${i} x ${j} = ${i * j}`);
+//  }
+// }
