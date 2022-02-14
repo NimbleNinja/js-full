433add791c73817399a23f31313230345c053694
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-02-14 17:07:38 +0200

    lesson 13 task 1 fix 1

--- lesson-13/task-1/index.test.js
+++ lesson-13/task-1/index.test.js
@@ -1,3 +1,5 @@
+const getEvenNumbers = arr => [...arr].filter(num => num % 2 === 0);
+
 it('17 равно 17', () => {
   expect(17).toEqual(17);
 });
@@ -6,8 +8,6 @@ it('18 не равно 17', () => {
   expect(18).not.toEqual(17);
 });
 
-const getEvenNumbers = arr => arr.filter(num => num % 2 === 0);
-
 it('should return even numbers', () => {
-  expect([2, 4, 6]).toEqual(getEvenNumbers([1, 2, 3, 4, 5, 6]));
+  expect(getEvenNumbers([1, 2, 3, 4, 5, 6])).toEqual([2, 4, 6]);
 });
