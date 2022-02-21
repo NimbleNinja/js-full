5d95bf10421daba3247a01c43f1e5740d8d37e43
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-02-21 15:48:44 +0200

    lesson 21, homework 6

---
+++ lesson-21/homework-6/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// algoritm
+// func clearList() => querySelector => innerHTML = ''
+
+// inp: none
+// return: undefined
+export function clearList() {
+  document.querySelector('.categories').innerHTML = '';
+}
