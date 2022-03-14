055f8b016018bd4e589326b555a83ab2c838e671
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-03-14 11:17:38 +0200

    lesson 33 task 1

---
+++ lesson-33/task-1/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// algoritm
+// get .name-form__btn, .user__avatar, .user__name, .user__location elements
+// add listener to showBtn
+// do request => use fetch(url).then(response => response.json())
+// get userData = > getUserData
+// process userData => processUserData
+
+const showBtnElem = document.querySelector('.name-form__btn');
+const inputElem = document.querySelector('.name-form__input');
+const userAvatarElem = document.querySelector('.user__avatar');
+const userNameElem = document.querySelector('.user__name');
+const userLocationElem = document.querySelector('.user__location');
+
+const userRequest = userName => {
+  const url = `https://api.github.com/users/${userName}`;
+
+  return fetch(url).then(response => renderUserData(response.json()));
+};
+
+const renderUserData = userData => {
+  userData.then(data => {
+    const { avatar_url, name, location } = data;
+    userAvatarElem.src = avatar_url;
+    userNameElem.textContent = name;
+    userLocationElem.textContent = location;
+  });
+};
+
+const onShowBtnClick = () => {
+  const inputValue = inputElem.value;
+
+  userRequest(inputValue);
+};
+
+showBtnElem.addEventListener('click', onShowBtnClick);
